1cfa9b6a144372c44e33bc2c3e71335214235d6a
Ezra Goldner <ezgoldner@gmail.com>
2024-09-12 14:08:42 -0400

    spotify workin on stats page

---
+++ hypr/position.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+LENGTH=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep -A 2 "length" | awk '{print $3}')
+TIME=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Position' |cut -d " " -f 12)
+echo $((TIME * 100 / LENGTH))
--- hypr/stats.sh
+++ hypr/stats.sh
@@ -4,10 +4,10 @@ POS=$(hyprctl cursorpos | awk -F "," '{print $1 $2}')
 X=$(echo $POS | awk '{print $1}')
 Y=$(echo $POS | awk '{print $2}')
 
-if [ "$X" -le "1373" ]; then
-    # eww update statsvisible=false
+if [ "$X" -le "1075" ]; then
+    eww update statsvisible=false
 fi
 
-if [ "$Y" -ge "410" ]; then
-    # eww update statsvisible=false
+if [ "$Y" -ge "365" ]; then
+    eww update statsvisible=false
 fi
