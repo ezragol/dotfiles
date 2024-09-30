3eea61a126852cc527a7e4bc0263fc19886b84f8
Ezra Goldner <ezgoldner@gmail.com>
2024-09-30 17:24:29 -0400

    okayyy. just gotta get the window opacity to reset when the stats window closes

--- hypr/statswitch.sh
+++ hypr/statswitch.sh
@@ -3,6 +3,10 @@
 STATUS=$(eww get statsvisible)
 
 if [ "$STATUS" = "true" ]; then
+    NEOWINDOW=$(hyprctl clients | awk -v RS= -v ORS='\n\n' "/class: neo/ {print $i}" | grep pid | awk '{print $2}')
+    TOPWINDOW=$(hyprctl clients | awk -v RS= -v ORS='\n\n' "/class: top/ {print $i}" | grep pid | awk '{print $2}')
+    kill $NEOWINDOW
+    kill $TOPWINDOW
     eww update statsvisible=false
     exec /home/ezra/.config/hypr/windows.sh 1.0 &
     eww close stats
@@ -10,4 +14,6 @@ else
     eww open stats
     exec /home/ezra/.config/hypr/windows.sh 0.0 &
     eww update statsvisible=true
+    alacritty --title "we are so back" --class top --hold -e top &
+    alacritty -o 'window.padding.y=47' -o 'window.padding.x=40' --class neo --hold -e neofetch &
 fi
