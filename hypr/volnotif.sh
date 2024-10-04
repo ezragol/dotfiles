4b33fe3fdcc405901690e1a9ab1236e1b6c21349
Ezra Goldner <ezgoldner@gmail.com>
2024-10-04 14:46:15 -0400

    idek anymore

--- hypr/volnotif.sh
+++ hypr/volnotif.sh
@@ -1,9 +1,10 @@
 #!/bin/zsh
 CURRENT_VOL=$1
 LAST=$(cat /home/ezra/.config/hypr/volid.txt)
+VOLP=$((CURRENT_VOL / 2))
 
 if [[ "$LAST" ]]; then
-    echo $(notify-send -t 3000 -a "volume" -h string:syncronous:volume -h int:value:$((CURRENT_VOL / 2)) " " -p -i /home/ezra/.config/hypr/images/volume_high.png -r $LAST) > /home/ezra/.config/hypr/volid.txt
+    echo $(notify-send -t 3000 -a "volume" -h string:syncronous:volume -h int:value:$VOLP "volume: $VOLP%" -p -r $LAST) > /home/ezra/.config/hypr/volid.txt
 else
-    echo $(notify-send -t 3000 -a "volume" -h string:syncronous:volume -h int:value:$((CURRENT_VOL / 2)) " " -p -i /home/ezra/.config/hypr/images/volume_high.png) > /home/ezra/.config/hypr/volid.txt
+    echo $(notify-send -t 3000 -a "volume" -h string:syncronous:volume -h int:value:$VOLP "volume: $VOLP%" -p) > /home/ezra/.config/hypr/volid.txt
 fi
